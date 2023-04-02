08f4775217a6e9c0e345b7d73cf99ac37d204e2c
yo08 <yosrmanai97@gmail.com>
2023-04-02 18:39:49 +0100

    reordering imports, handling the image path from input argument

--- abnormality_detection/abnormality_detection.py
+++ abnormality_detection/abnormality_detection.py
@@ -1,5 +1,7 @@
-"""Module Description"""
-from typing import Any, Dict, List
+"""Module Description."""
+import json
+from pathlib import Path
+from typing import Any
 
 import numpy as np
 import torch
@@ -7,50 +9,48 @@ from PIL import Image
 
 from grasping import infer as grasping_infer
 from src.networks.build_network import build_model as build_grasping_model
-from pathlib import Path
-import json
 
 
-def load_model(model_path: Path, device: torch.device) -> Dict[str, Any]:
+def load_model(model_path: Path, device: torch.device) -> dict[str, Any]:
     # TODO load pytorch model weights
-    with open(model_path.joinpath('obj', 'build_parameter.json'), encoding='utf-8') as f:
+    with open(model_path.joinpath("obj", "build_parameter.json"), encoding="utf-8") as f:
         build_parameters = json.load(f)
     return _load_grasping(model_path, build_parameters, device)
 
 
 def _load_grasping(
-        path: Path, build_parameters: Dict[str, Any], device: torch.device) -> Dict[str, Any]:
-    input_width = build_parameters['input_width']
-    input_height = build_parameters['input_height']
-    nb_classes = build_parameters['nb_classes']
-    subdivs = build_parameters['subdivs']
-    network_name = build_parameters['network']
-    dim_mins = tuple(build_parameters['dim_mins'])
-    dim_maxs = tuple(build_parameters['dim_maxs'])
+        path: Path, build_parameters: dict[str, Any], device: torch.device) -> dict[str, Any]:
+    input_width = build_parameters["input_width"]
+    input_height = build_parameters["input_height"]
+    nb_classes = build_parameters["nb_classes"]
+    subdivs = build_parameters["subdivs"]
+    network_name = build_parameters["network"]
+    dim_mins = tuple(build_parameters["dim_mins"])
+    dim_maxs = tuple(build_parameters["dim_maxs"])
 
-    network_path = path.joinpath('obj', 'model.pth')
+    network_path = path.joinpath("obj", "model.pth")
     network = build_grasping_model(network_name, model_path=network_path, eval_mode=True,
                                    image_sizes=(input_height, input_width), nb_classes=nb_classes, subdivs=subdivs)
     network = network.to(device)
 
     return {
-        'method': 'grasping',
-        'network': network,
-        'input_width': input_width,
-        'input_height': input_height,
-        'dim_mins': dim_mins,
-        'dim_maxs': dim_maxs,
+        "method": "grasping",
+        "network": network,
+        "input_width": input_width,
+        "input_height": input_height,
+        "dim_mins": dim_mins,
+        "dim_maxs": dim_maxs,
     }
 
 
-def grasping_inference(model: Dict[str, Any], image: Image.Image,
-                       device: torch.device) -> Dict[str, Any]:
-    #     # TODO implement Grasping Inference Call
-    #     # It should return a list with the detected objects centers
+def grasping_inference(model: dict[str, Any], image: Image.Image,
+                       device: torch.device) -> dict[str, Any]:
+    # implement Grasping Inference Call
+    # It returns a list with the detected objects centers
     # return grasping_infer(model, image, device)
     prediction_points, prediction_image = grasping_infer.infer(
-        model['network'], image, model['input_width'], model['input_height'], device, visualization=False,
-        dim_mins=model['dim_mins'], dim_maxs=model['dim_maxs']
+        model["network"], image, model["input_width"], model["input_height"], device, visualization=False,
+        dim_mins=model["dim_mins"], dim_maxs=model["dim_maxs"],
     )
     return prediction_points
 
@@ -64,13 +64,13 @@ def is_allowed(x: float, y: float, allowed_regions: np.ndarray) -> bool:
     return allowed_regions[round(x)][round(y)] != np.NaN
 
 
-def judge_positions(positions: Dict[str, Any], allowed_regions: np.ndarray) -> List[Dict[str, Any]]:
+def judge_positions(positions: dict[str, Any], allowed_regions: np.ndarray) -> list[dict[str, Any]]:
     # TODO implement method
     # It should return a list with the positions with the addition of a new key: "allowed_region": true/false
-    raise NotImplementedError()
+    raise NotImplementedError
 
 
-def judge_image(model: Dict[str, Any], image: Image.Image, allowed_regions: np.ndarray, device: torch.device
-                ) -> List[Dict[str, Any]]:
+def judge_image(model: dict[str, Any], image: Image.Image, allowed_regions: np.ndarray, device: torch.device,
+                ) -> list[dict[str, Any]]:
     detected_objects = grasping_inference(model, image, device)
     return judge_positions(detected_objects, allowed_regions)
--- test_abnormality_detection.py
+++ test_abnormality_detection.py
@@ -1,35 +1,39 @@
 import argparse
+from pathlib import Path
 
 import torch
-
 from abnormality_detection import abnormality_detection
-
 from PIL import Image
-import numpy as np
 
 
 def parse_args() -> argparse.Namespace:
     """Parse command line arguments.
 
-    Returns:
+    Returns
+    -------
         instance that has parsed arguments.
     """
-    parser = argparse.ArgumentParser('Perform 2d picking model inference and over an image and judge whether'
-                                     'the detected objects are within allowed area')
-    parser.add_argument('model_path', type=str, help='Path to  the trained 2d picking model weights')
-    parser.add_argument('image_path', type=str, help='Path to the image to be tested')
-    parser.add_argument('allowed_regions', type=str, help='Path to an image describing the allowed regions.'
-                        ' It must contain only black and white pixels, black meaning not allowed regions.'
-                        ' It must have the same dimensions as the input image')
+    parser = argparse.ArgumentParser("Perform 2d picking model inference and over an image and judge whether"
+                                     "the detected objects are within allowed area")
+    parser.add_argument(
+        "model_path",
+        type=str,
+        help="Path to  the trained 2d picking model weights")
+    parser.add_argument("image_path", type=str, help="Path to the image to be tested")
+    parser.add_argument("allowed_regions", type=str, help="Path to an image describing the allowed regions."
+                        " It must contain only black and white pixels, black meaning not allowed regions."
+                        " It must have the same dimensions as the input image")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
 
-    # TODO handle device properly (choose device from command line, check if cuda is available)
-    device = torch.device('cuda')
-    grasping_model = abnormality_detection.load_model(args.model_path, device)
+    # handle device
+    device = torch.device("cuda")
+    grasping_model = abnormality_detection.load_model(Path(args.model_path), device)
+    print(grasping_model)
+    image = Image.open(args.image_path)
 
     # TODO load image (As a PIL.Image) from path, load allowed regions from path (as np.ndarray) and pass them as
     # arguments for judge_image()
