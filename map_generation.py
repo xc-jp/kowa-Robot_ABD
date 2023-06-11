5916e411a68ca9d88aa0efc19fc613e239268dcc
yo08 <yosrmanai97@gmail.com>
2023-06-11 19:51:36 +0200

    create map generation file with short description

---
+++ map_generation.py
@@ -0,0 +1,8 @@
+#  ***** DESCRIPTION *****
+# This script generates the map of alowed regions in .png format.
+# The map is created based on a given video that is used as reference
+# Also, the allowed regions are mapped depending on a variety of inputs specified by the user
+# For the moment, we only take a radius as an input argument
+
+
+# UNDER CONSTRUCTION
